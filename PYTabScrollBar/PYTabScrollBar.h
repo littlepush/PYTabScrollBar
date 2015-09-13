edf268cb99df32a3ae30fa84955ff64237bcc493
Push Chen <littlepush+home.macmini@gmail.com>
2015-09-14 01:12:31 +0800

    init the project

---
+++ PYTabScrollBar/PYTabScrollBar.h
@@ -0,0 +1,14 @@
+//
+//  PYTabScrollBar.h
+//  PYTabScrollBar
+//
+//  Created by Push Chen on 9/14/15.
+//  Copyright (c) 2015 PushLab. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface PYTabScrollBar : UIView
+
+@end
---
+++ PYTabScrollBar/PYTabScrollBar.m
@@ -0,0 +1,13 @@
+//
+//  PYTabScrollBar.m
+//  PYTabScrollBar
+//
+//  Created by Push Chen on 9/14/15.
+//  Copyright (c) 2015 PushLab. All rights reserved.
+//
+
+#import "PYTabScrollBar.h"
+
+@implementation PYTabScrollBar
+
+@end
