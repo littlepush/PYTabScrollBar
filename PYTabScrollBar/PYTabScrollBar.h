7fb829c5a67c422e25a1eb2bd8dc11c2b88a954f
Push Chen <littlepush+home.macmini@gmail.com>
2015-09-18 02:07:19 +0800

    update the tab scroll bar's definition

--- PYTabScrollBar/PYTabScrollBar.h
+++ PYTabScrollBar/PYTabScrollBar.h
@@ -11,4 +11,35 @@
 
 @interface PYTabScrollBar : UIView
 
+/*!
+ @brief all tab views in the container.
+ */
+@property (nonatomic, readonly) NSArray             *tabs;
+
+/*!
+ @brief current selected tab view.
+ */
+@property (nonatomic, readonly) UIView              *selectedTab;
+
+/*!
+ @brief The max available width while the container has enough space to display
+ the full size tab item.
+ */
+@property (nonatomic, assign)   CGFloat             maxTabWidth;
+
+/*!
+ @brief The min width to display a full tab item.
+ */
+@property (nonatomic, assign)   CGFloat             minTabWidth;
+
+/*!
+ @brief the left side customized view
+ */
+@property (nonatomic, strong)   UIView              *leftView;
+
+/*!
+ @brief the right side customized view
+ */
+@property (nonatomic, strong)   UIView              *rightView;
+
 @end
--- PYTabScrollBar/PYTabScrollBar.m
+++ PYTabScrollBar/PYTabScrollBar.m
@@ -8,6 +8,135 @@
 
 #import "PYTabScrollBar.h"
 
+@interface PYTabScrollBar () <UIGestureRecognizerDelegate>
+{
+    UIView                          *_tabContainer;
+    UIPanGestureRecognizer          *_panGesture;
+    
+    NSMutableArray                  *_tabPool;
+    NSInteger                       _selectedIndex;
+    
+    // Left and right view
+    UIView                          *_leftView;
+    UIView                          *_rightView;
+}
+
+@end
+
 @implementation PYTabScrollBar
+
+@synthesize tabs = _tabPool;
+@dynamic selectedTab;
+- (UIView *)selectedTab
+{
+    @synchronized(self) {
+        if ( _tabPool.count <= _selectedIndex ) return nil;
+        return _tabPool[_selectedIndex];
+    }
+}
+
+@synthesize leftView = _leftView;
+- (void)setLeftView:(UIView *)leftView
+{
+    [self willChangeValueForKey:@"leftView"];
+    
+    if ( _leftView != nil && _leftView.superview != nil ) {
+        [_leftView removeFromSuperview];
+    }
+    
+    _leftView = leftView;
+    [self addSubview:_leftView];
+    [_leftView setCenter:CGPointMake(_leftView.bounds.size.width / 2,
+                                     self.bounds.size.height / 2)];
+    
+    // Re-layout the tab container
+    [self setNeedsLayout];
+    
+    [self didChangeValueForKey:@"leftView"];
+}
+
+@synthesize rightView = _rightView;
+- (void)setRightView:(UIView *)rightView
+{
+    [self willChangeValueForKey:@"rightView"];
+    
+    if ( _rightView != nil && _rightView.superview != nil ) {
+        [_rightView removeFromSuperview];
+    }
+    
+    _rightView = rightView;
+    [self addSubview:_rightView];
+    [_leftView setCenter:CGPointMake(_rightView.bounds.size.width / 2,
+                                     self.bounds.size.height / 2)];
+    
+    [self setNeedsLayout];
+    
+    [self didChangeValueForKey:@"rightView"];
+}
+
+- (void)_initialize_tabbar
+{
+    // Data Initial
+    self.maxTabWidth = 200.f;
+    self.minTabWidth = 80.f;
+    _leftView = nil;
+    _rightView = nil;
+    _selectedIndex = 0;
+    _tabPool = [NSMutableArray array];
+    
+    // Create the container
+    _tabContainer = [[UIView alloc] init];
+    [self addSubview:_tabContainer];
+    
+    // Add the main gesture
+    _panGesture = [[UIPanGestureRecognizer alloc]
+                   initWithTarget:self
+                   action:@selector(_action_pangesture_handler:)];
+    _panGesture.delegate = self;
+    [_tabContainer addGestureRecognizer:_panGesture];
+}
+
+- (void)_process_ScrollAllTabsWithDeltaDistance:(CGFloat)distance animationTime:(CGFloat)duration
+{
+    
+}
+
+- (void)_action_pangesture_handler:(UIPanGestureRecognizer *)panGesture
+{
+    // Calculate the speed and move
+}
+
+- (BOOL)gestureRecognizerShouldBegin:(UIGestureRecognizer *)gestureRecognizer
+{
+    // Stop current animation first
+    
+    // Save the begin point.
+    return YES;
+}
+
+- (void)layoutSubviews
+{
+    [super layoutSubviews];
+    
+    // Re-layout all opened tabs
+}
+
+- (id)init
+{
+    self = [super init];
+    if ( self ) {
+        [self _initialize_tabbar];
+    }
+    return self;
+}
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if ( self ) {
+        [self _initialize_tabbar];
+    }
+    return self;
+}
 
 @end
